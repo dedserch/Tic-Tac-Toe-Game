af61ea3bedf76d2b2806c7b32c62d75b8ab7926b
dedserch <dedvetal@mail.ru>
2024-02-23 21:30:51 +0300

    fix bug with currentPlayer

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
 const cells = Array.from(document.querySelectorAll('.cells'))
 const restartBtn = document.querySelector('.restart')
-const players = new Map()
-players.set("FirstPlayer", "X")
-players.set("SecondPlayer", "O")
+const players = new Map([
+    ["FirstPlayer", "X"],
+    ["SecondPlayer", "O"],
+])
 let currentPlayer = players.get("FirstPlayer")
-let freeCells = Array(9).fill(null)
+let freeCells = new Array(9).fill(null)
+
 const combinationToWin = [
-    [0,1,2],[3,4,5],[6,7,8],
-    [0,3,5],[1,4,7],[2,5,8],
-    [0,4,8],[2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 5],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ]
 
-const startGame = ()=>{
-    cells.forEach( cells => cells.addEventListener("click",cellsClick))
+const startGame = () => {
+    cells.forEach(cell => cell.addEventListener("click", cellClick))
 }
 
-const cellsClick = (e) =>{
+const cellClick = (e) => {
     const id = e.target.id
-    if(!freeCells[id]){
-        freeCells[id] = currentPlayer
-        e.target.innerText = currentPlayer
+
+    if (freeCells[id]) {
+        return
     }
-    if(checkWinner() != false){
-        alert(`Выиграл игрок с символом ${currentPlayer}`)
+
+    freeCells[id] = currentPlayer
+    e.target.innerText = currentPlayer
+
+    const winner = checkWinner();
+    if (winner) {
+        alert(`Победитель: ${currentPlayer}`)
+        return
     }
-    else if (freeCells.every(cells => cells != null) &&  !checkWinner()){
+    if (freeCells.every(cell => cell !== null)) {
         alert("Ничья")
+        return
     }
-    
-    currentPlayer = currentPlayer == players.get("FirstPlayer")? players.get("SecondPlayer"):players.get("FirstPlayer") 
+
+    currentPlayer = currentPlayer === players.get("FirstPlayer") ? players.get("SecondPlayer") : players.get("FirstPlayer")
 }
 
-const checkWinner = () =>{
-    for(let i = 0; i<combinationToWin.length; i++){
+const checkWinner = () => {
+    for (let i = 0; i < combinationToWin.length; i++) {
         const combination = combinationToWin[i]
-        let [a,b,c] = combination
-        if(freeCells[a] && freeCells[a] == freeCells[b] && freeCells[a] == freeCells[c]){
-            return [a,b,c]
+        const [a, b, c] = combination
+        if (freeCells[a] && freeCells[a] === freeCells[b] && freeCells[a] === freeCells[c]) {
+            return combination
         }
     }
     return false
 }
 
-
-restartBtn.addEventListener("click", ()=>{
+restartBtn.addEventListener("click", () => {
     freeCells.fill(null)
     currentPlayer = players.get("FirstPlayer")
-    cells.forEach(cells => cells.innerHTML = '')
+    cells.forEach(cell => cell.innerHTML = "")
 })
-
-
 startGame()
-
-
-
